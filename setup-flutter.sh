740b4e4b111e494768d2c3f83d3da7dfbaddc804
Jakob Borg <jakob@kastelo.net>
2020-12-30 22:56:16 +0100

    wip

---
+++ setup-flutter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version=1.22.4-stable
+sdk=~/sdk/flutter-"$version"
+
+if [[ ! -d "$sdk" ]] ; then
+    rm -f flutter.zip
+    curl -L -o flutter.zip "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_${version}.zip"
+
+    mkdir -p "$sdk"
+    unzip flutter.zip -d "$sdk"
+fi
+
+export PATH="$sdk/flutter/bin:$PATH"
+echo "export PATH=\"$sdk/flutter/bin:\$PATH\""
